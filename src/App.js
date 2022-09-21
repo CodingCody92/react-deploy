261bd9cbebce2eca67de1aa5bb29f1933a2e3706
Cody <alexhill318@yahoo.com>
2022-09-21 12:41:30 -0500

    deployed react app to github

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='begin'>
+      <h1>Hi, its my first react deployment</h1>
+    <h2>REACT!!!</h2>
+    <div className='img'>
+      <img src="https://reactjs.org/logo-og.png" alt="" />
+    </div>
+    </div>
+  )
+}
+
+export default App;
